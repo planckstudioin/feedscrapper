8bf3b4a484546d01cd5bd1638c8794e4de799ac3
Yash Gohel <yashgohel16@gmail.com>
2022-04-19 18:41:52 +0530

    scrapper-freepik api

---
+++ main.py
@@ -0,0 +1,48 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+from flask_cors import CORS
+
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
+
+class Scrapper(Resource):
+    def get(self):
+
+        u = request.args.get('query')
+        p = request.args.get('page')
+
+        url = "https://www.freepik.com/search?format=search&orientation=square&page=" + \
+            str(p) + "&query=" + u
+
+        headers = {
+            'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
+        }
+
+        response = requests.request("GET", url, headers=headers)
+        data = BeautifulSoup(response.text, 'html.parser')
+        # find all with the image tag
+        images = data.find_all('img', src=True, class_="lzy")
+        print(images)
+        image_src = [x['data-src'] for x in images]
+        img = []
+
+        for i in image_src:
+            iu = i.split(".jpg")
+            img.append(iu[0] + ".jpg")
+
+        res = {
+            "img": img,
+            "keywords": u
+        }
+
+        text = json.dumps(res, sort_keys=True, indent=4)
+        return res
+
+api.add_resource(Scrapper,'/freepik/', methods = ['GET'])
+
+if __name__ == '__main__':
+    app.run()
